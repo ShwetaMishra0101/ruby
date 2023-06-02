220d2e06ba74e01d6006163a005af1f4f60e9f37
ShwetaMishra0101 <93873372+ShwetaMishra0101@users.noreply.github.com>
2023-06-02 11:04:51 +0530

    prit methods

--- the_puts_methods.rb
+++ the_puts_methods.rb
@@ -1,15 +1,21 @@
-puts 4
-puts 5
+# puts 4
+# puts 5
 
-puts 4,5 
+# puts 4,5 
 
 
-# it escape characters
-# beacuse we escape traditional characters
+# # it escape characters
+# # beacuse we escape traditional characters
 
-puts  "hello \n world"
-puts "\tHello world"
+# puts  "hello \n world"
+# puts "\tHello world"
 
-puts "juliet said \"goodbye\" Romeo "
-puts "juliet said \'goodbye' Romeo "
+# puts "juliet said \"goodbye\" Romeo "
+# puts "juliet said \'goodbye' Romeo "
+
+
+# print methods 
+
+print "this random"
+print " will continue the same "
 
