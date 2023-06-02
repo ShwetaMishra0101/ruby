d4f6603a5a639fcfcea91221178ec003ba243068
ShwetaMishra0101 <93873372+ShwetaMishra0101@users.noreply.github.com>
2023-06-02 10:59:05 +0530

    escape characters

---
+++ the_puts_methods.rb
@@ -0,0 +1,15 @@
+puts 4
+puts 5
+
+puts 4,5 
+
+
+# it escape characters
+# beacuse we escape traditional characters
+
+puts  "hello \n world"
+puts "\tHello world"
+
+puts "juliet said \"goodbye\" Romeo "
+puts "juliet said \'goodbye' Romeo "
+
