6df2219a6eb850e36d13769734aef73c8b27fa7f
ShwetaMishra0101 <93873372+ShwetaMishra0101@users.noreply.github.com>
2023-06-04 23:25:29 +0530

    while loop

---
+++ 10 - Loops/the_while_loop.rb
@@ -0,0 +1,22 @@
+
+
+#loop a series of instructions that is repeated until A
+# terminating cond is reached
+
+# while loop excute wlie a condition is true
+count = 1
+
+while count <10
+
+puts "hello"
+count +=1
+
+  
+end
+puts 
+latteers ="a"
+
+while latteers.length<5
+    puts latteers
+    latteers << "a"
+    end
