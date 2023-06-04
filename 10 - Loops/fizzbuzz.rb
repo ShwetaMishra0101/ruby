926246ee177e2b16038f92216900dc3ae22b895f
ShwetaMishra0101 <93873372+ShwetaMishra0101@users.noreply.github.com>
2023-06-04 23:41:30 +0530

    fizz buzz problem

---
+++ 10 - Loops/fizzbuzz.rb
@@ -0,0 +1,23 @@
+def fizzbuzz(num)
+i=1
+    while i <= num
+    if i % 3 == 0 && i%5 == 0
+        puts "fizzbuz"
+    elsif i % 3 == 0
+          puts  "fizz"
+           
+          elsif i%5==0
+            puts"BUZZ"
+               
+            else
+                puts i
+                
+            end
+            i +=1
+
+    end
+
+
+end
+
+fizzbuzz(30)
