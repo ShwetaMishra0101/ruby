96fd81148e6707c5acccd30096cd846cd6779af8
ShwetaMishra0101 <93873372+ShwetaMishra0101@users.noreply.github.com>
2023-06-12 12:24:46 +0530

    class

--- ruby_project/r.rb
+++ ruby_project/r.rb
@@ -1,40 +1,75 @@
 
-user = [
-    {name:"shweta", password:"shweta"},
-    {name:"shyam", password:"shyam"},
-    {name:"seeta", password:"seeta"},
-    {name:"shw", password:"shw"}
+# user = [
+#     {name:"shweta", password:"shweta"},
+#     {name:"shyam", password:"shyam"},
+#     {name:"seeta", password:"seeta"},
+#     {name:"shw", password:"shw"}
 
-]
+# ]
 
-def auth_user(name, password, list_of_user)
-    list_of_user.each do |user_record|
-        if user_record[:name] == name && user_record[:password] == password
+# def auth_user(name, password, list_of_user)
+#     list_of_user.each do |user_record|
+#         if user_record[:name] == name && user_record[:password] == password
 
-            return user_record
-        end
+#             return user_record
+#         end
+#     end
+#     "credentials were not correct"
+
+# end
+
+# puts "welcole to the auth"
+
+# attemps = 1 
+# while attemps< 4
+#     print "Username: "
+#   name = gets.chomp
+#   print "Password: "
+#   password = gets.chomp
+
+#  authentication = auth_user(name, password, user)
+
+#   puts authentication
+#   puts "Press n to quit or any other key to continue: "
+#   input = gets.chomp.downcase
+#   break if input == "n"
+
+
+# end
+# puts "you have exceeded the number of attempts " if attemps == 4
+
+
+# area code
+
+dail_book= {
+    "basti" => "272131",
+    "nagar" => "122",
+    "lucknow" => "2078",
+    "kanpur" => "208024"
+}
+
+def get_city_nane(somehash)
+    somehash.keys
+end
+
+def get_area_code(somehash, key)
+    somehash[key]
+end
+
+
+loop do
+    puts "enter area code?(Y/N)"
+    ans = gets.chomp.downcase
+    break if ans != "y"
+    puts "which city do you want the area code for?"
+    puts get_city_nane(dail_book)
+    puts "Enter you selection"
+
+    prompt = gets.chomp
+
+    if dail_book.include?(prompt)
+        puts "the area code for #{prompt} is #{get_area_code(dail_book,prompt)}"
+    else
+        puts "you entered a city name not in the dictionary"
     end
-    "credentials were not correct"
-
 end
-
-puts "welcole to the auth"
-
-attemps = 1 
-while attemps< 4
-    print "Username: "
-  name = gets.chomp
-  print "Password: "
-  password = gets.chomp
-
- authentication = auth_user(name, password, user)
-
-  puts authentication
-  puts "Press n to quit or any other key to continue: "
-  input = gets.chomp.downcase
-  break if input == "n"
-
-
-end
-puts "you have exceeded the number of attempts " if attemps == 4
-
---
+++ ruby_project/stud_class.rb
@@ -0,0 +1,55 @@
+class Student
+# attribute
+
+attr_accessor :first_name, :last_name, :username, :email, :password 
+# attr_reader :Username
+# @first_name
+# @last_name
+# @email
+# @password
+# @username
+
+def initialize(first_name,last_name,username,email,password)
+    @first_name = first_name
+    @last_name = last_name
+    @Username= username
+    @email = email
+    @password = password
+end
+
+
+# def first_name =(name)
+#     @first_name = name
+# end
+
+# def first_name
+#     @first_name
+# end
+
+
+# defalut do_s
+def to_s 
+    "First name: #{@first_name}, Last_name : #{@last_name}, UserName : #{@username}, email address : #{email}"
+    end
+
+
+end
+
+# new obj
+
+# shw = Student.new
+# puts shw
+# shw.first_name = "shweta"
+# shw.last_name = "mishra"
+# shw.email = "shw@gmail.com"
+
+# puts shw
+# puts shw.last_name
+# puts shw.email
+
+
+
+
+shw = Student.new("Shweta","Mishra","shwe","shw@gmail.com","1234")
+
+puts shw
